fcff2b374b5543cf13b8bba24d7bd0de817baa51
jbeedle19 <josh.beedle@gmail.com>
2021-01-27 10:51:24 -0500

    refactored upvote code

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -77,34 +77,13 @@ router.post('/', (req, res) => {
 
 // add an upvote to a post with PUT
 router.put('/upvote', (req, res) => {
-    Vote.create({
-        user_id: req.body.user_id,
-        post_id: req.body.post_id
-    }).then(() => {
-        // Then find the post we just voted on
-        return Post.findOne({
-            where: {
-                id: req.body.post_id
-            },
-            attributes: [
-                'id',
-                'post_url',
-                'title',
-                'created_at',
-                // Use raw MySQL aggregate function query to get a count of how many
-                // votes the post has and return it under the name 'vote_count'
-                [
-                    sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
-                    'vote_count'
-                ]
-            ]
-        })
-        .then(dbPostData => res.json(dbPostData))
-        .catch(err => {
-            console.log(err);
-            res.status(400).json(err);
-        });
-    })
+    // custom static method created in models/Post.js
+    Post.upvote(req.body, { Vote })
+    .then(updatedPostData => res.json(updatedPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+    });
 });
 
 // Update a post's title with PUT
