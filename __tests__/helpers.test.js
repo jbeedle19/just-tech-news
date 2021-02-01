34edffcca5b92992c16573ec09b3a48a34ca87c6
jbeedle19 <josh.beedle@gmail.com>
2021-02-01 14:49:50 -0500

    wrote/fulfilled test for URL formats

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -1,4 +1,4 @@
-const { format_date, format_plural } = require('../utils/helpers');
+const { format_date, format_plural, format_url } = require('../utils/helpers');
 
 test('format_date() returns a date string', () => {
     const date = new Date('2021-02-01 14:19:03');
@@ -10,4 +10,14 @@ test('format_plural() returns pluralized words', () => {
 
     expect(format_plural('tiger', 2)).toBe('tigers');
     expect(format_plural('lion', 1)).toBe('lion');
+});
+
+test('format_url() returns a simplified url string', () => {
+    const url1 = format_url('http://test.com/page/1');
+    const url2 = format_url('https://www.coolstuff.com/abcdefg/');
+    const url3 = format_url('https://www.google.com?q=hello');
+
+    expect(url1).toBe('test.com');
+    expect(url2).toBe('coolstuff.com');
+    expect(url3).toBe('google.com');
 });
