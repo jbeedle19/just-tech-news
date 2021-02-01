2f9d60b2915bc9f15b453b48cfbd2d3c3e9c548c
jbeedle19 <josh.beedle@gmail.com>
2021-02-01 14:42:13 -0500

    wrote/fulfilled test for plural words

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -1,7 +1,13 @@
-const {format_date} = require('../utils/helpers');
+const { format_date, format_plural } = require('../utils/helpers');
 
 test('format_date() returns a date string', () => {
     const date = new Date('2021-02-01 14:19:03');
 
     expect(format_date(date)).toBe('2/1/2021');
+});
+
+test('format_plural() returns pluralized words', () => {
+
+    expect(format_plural('tiger', 2)).toBe('tigers');
+    expect(format_plural('lion', 1)).toBe('lion');
 });
